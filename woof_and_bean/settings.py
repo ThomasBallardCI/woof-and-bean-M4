b1241b247d6a55db6c07e28f2fd6c4e5a70aff35
ThomasBallardCI <ThomasGEBallard@gmail.com>
2024-03-10 20:21:01 +0000

    adjusted heroku allowed host name

--- woof_and_bean/settings.py
+++ woof_and_bean/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['woof-and-bean-ci-m4-3f6e86d7432d.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://woof-and-bean-ci-m4-3f6e86d7432d.herokuapp.com/', 'localhost']
 
 
 # Application definition
