7bba8944d76633d8b0740aad7a08aa55022768b1
ThomasBallardCI <ThomasGEBallard@gmail.com>
2024-03-15 13:56:54 +0000

    additions to readme and flake8 formatting

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 urlpatterns = [
     path("", views.profile, name="profile"),
-    path("order_history/<order_number>", views.order_history, name="order_history"),
     path(
-        "add_to_wishlist/<product_id>/", views.add_to_wishlist, name="add_to_wishlist"
+        "order_history/<order_number>",
+        views.order_history,
+        name="order_history",
+    ),
+    path(
+        "add_to_wishlist/<product_id>/",
+        views.add_to_wishlist,
+        name="add_to_wishlist",
     ),
     path(
         "remove_from_wishlist/<product_id>/",
